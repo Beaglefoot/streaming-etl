02c58ac0bf3b7b872c5768c697e96d696266df4d
Beaglefoot <ch-e-st@yandex.ru>
2022-04-28 19:55:42 +0300

    Set up staging db

---
+++ streaming-etl/services/staging-db/init-scripts/01_create_user_lookup.sql
@@ -0,0 +1,4 @@
+CREATE TABLE user_lookup (
+    user_id int PRIMARY KEY NOT NULL,
+    user_key int NOT NULL
+);
---
+++ streaming-etl/services/staging-db/init-scripts/02_create_room_lookup.sql
@@ -0,0 +1,4 @@
+CREATE TABLE room_lookup (
+    room_id int PRIMARY KEY NOT NULL,
+    room_key int NOT NULL
+);
